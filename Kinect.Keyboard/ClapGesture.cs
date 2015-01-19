5b73f7d03421c1f41944fea766c8b4e0187e3a64
Jan Saris <jan.saris@gmail.com>
2015-01-19 21:44:13 +0100

    Implemented ClapGesture

--- Kinect.Keyboard/ClapGesture.cs
+++ Kinect.Keyboard/ClapGesture.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Kinect.Gestures;
 using log4net;
 using Microsoft.Kinect;
@@ -8,7 +9,13 @@ namespace Kinect.Keyboard
     public class ClapGesture : GestureBase
     {
         private static readonly ILog Logger = LogManager.GetLogger(typeof(ClapGesture));
+        private enum State { Open, Closed }
 
+        private const int MaxFrames = 6;
+        private const float MaxDistance = 0.05f;
+
+        private int _counter = MaxFrames;
+        private State _previousStateFrame = State.Open;
 
         public ClapGesture()
         {
@@ -28,7 +35,36 @@ namespace Kinect.Keyboard
 
         private bool IsClapFinished(Body body)
         {
-            return false;
+            var closed = IsClosed(body);
+            if (closed)
+            {
+                _counter--;
+            }
+            ResetCounter();
+            
+            var clapFinished = !closed && _counter < MaxFrames && _counter >= 0;
+            var newState = closed ? State.Closed : State.Open;
+            Logger.DebugFormat("Prev: {0}; New: {1}; Count: {2}, Finish: {3}", _previousStateFrame, newState, _counter, clapFinished);
+
+            _previousStateFrame = newState;
+            ResetCounter();
+            
+            return clapFinished;
+        }
+
+        private void ResetCounter()
+        {
+            if (_previousStateFrame == State.Open)
+            {
+                _counter = MaxFrames;
+            }
+        }
+
+        private static bool IsClosed(Body body)
+        {
+            var left = body.Joints[JointType.HandLeft].Position;
+            var right = body.Joints[JointType.HandRight].Position;
+            return Math.Abs(left.X - right.X) < MaxDistance && Math.Abs(left.Y - right.Y) < MaxDistance;
         }
     }
 }
